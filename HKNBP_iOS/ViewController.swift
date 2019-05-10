e442412104367720f5762b21111057018de4c4d9
iHad169 <27337315+iHad169@users.noreply.github.com>
2019-05-10 13:47:07 +0800

    upload

---
+++ HKNBP_iOS/ViewController.swift
@@ -0,0 +1,47 @@
+/*
+ * HKNBP_iOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HKNBP_iOS is distributed in the hope that it will be useful,
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HKNBP_iOS.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.configuration.allowsInlineMediaPlayback = true
+        webView.configuration.allowsAirPlayForMediaPlayback = true
+        webView.configuration.allowsPictureInPictureMediaPlayback = true
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17" //Desktop Mode
+        let myURL = URL(string:"https://hknbp.org")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
